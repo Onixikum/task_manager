90e8da99a35677cc6995045a69681c28e79e0c80
OniX <dragonix1988@gmail.com>
2016-07-11 15:47:23 +0300

    Add style

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+  subject { page }
 
-    it "should have the content" do
-      visit root_path
-      expect(page).to have_content('StaticPages')
-    end
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('Home') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('|') }
   end
 end
