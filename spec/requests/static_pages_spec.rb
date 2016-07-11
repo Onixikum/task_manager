94d60a333e5bfda73202d64dbdee12254649c94a
OniX <dragonix1988@gmail.com>
2016-07-11 19:34:02 +0300

    Make a basic User model (including secure password)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,8 +7,21 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Home') }
+    it { should have_selector('h1', 'Welcom to the Task Manager') }
     it { should have_title(full_title('')) }
     it { should_not have_title('|') }
+  end
+
+  it "shoud have the right links on the layout" do
+    visit root_path
+    first('.container').click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    first('.container').click_link "TaskManager"
+    expect(page).to have_title(full_title(''))
+    first('.footer').click_link "TaskManager"
+    expect(page).to have_title(full_title(''))
+    first('.footer').click_link "Home"
+    expect(page).to have_title(full_title(''))
   end
 end
